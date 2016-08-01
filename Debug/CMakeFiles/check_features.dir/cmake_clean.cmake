fbc1d707f2b2e6a1e5c15fba3a68ac8aeb0729b4
zeus <carlospark@ifunfactory.com>
2016-08-01 16:29:37 +0900

    gitignore
    Change-Id: Iac7998623425afe404ab5cfc0c9f17c0fed3fc8e

--- Debug/CMakeFiles/check_features.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "_CPack_Packages"
-  "CMakeFiles/check_features"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/check_features.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
